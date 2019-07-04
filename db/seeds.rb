0d6f86faef7f3c63909bb834b9e515d1607df124
Alisia Michel <cc0-11@outlook.de>
2019-07-04 19:49:28 +0200

    nicer layout

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'json'
+require 'open-uri'
+
+Ingredient.destroy_all
+
+puts "Parsing..."
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+ingredient_serialized = open(url).read
+ingredients = JSON.parse(ingredient_serialized)
+
+puts "Add ingredients"
+
+ingredients.each do |drink|
+  drink[1].each do |ing|
+    ingr = Ingredient.create(name: "#{ing["strIngredient1"]}")
+  end
+end
+
+puts "Finished"
